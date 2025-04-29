c5ed35b50ba2ed992e307b0f0f323b53c29ea382
aseelalatabi3 <aseelalatabi3@gmail.com>
2025-04-29 15:33:16 +0300

    Complete Assignment 8 - ERC721 NFT contract

--- test/Assignment8.t.sol
+++ test/Assignment8.t.sol
@@ -1,33 +1,22 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import {Test} from "forge-std/Test.sol";
-import {Assignment8} from "../src/Assignment8.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract Assignment8Test is Test {
-    Assignment8 assignment;
-    string public temporaryUriForTesting = "https://azure-yearning-shrew-339.mypinata.cloud/ipfs/bafkreifo3hg2hnig5ojay3tonmf6un3qlxqjienffsmpkq5cfpept2fake";
+// العقد يرث من ERC721URIStorage و Ownable
+contract Assignment8 is ERC721URIStorage, Ownable {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIdCounter;
 
-    function setUp() public {
-        assignment = new Assignment8();
-    }
+    constructor() ERC721("MyNFT", "MNFT") {}
 
-    function testMintNFTByOwner() public {
-        // Mint an NFT to the test contract (owner calling)
-        assignment.mintNFT(temporaryUriForTesting);
-
-        // Check ownership of the minted NFT
-        address owner = assignment.ownerOf(0);
-        assertEq(owner, address(this), "Owner of token ID 1 should be the test contract");
-    }
-
-    function testMintNFTByNonOwner() public {
-        // Create a non-owner address
-        address nonOwner = makeAddr("nonOwner");
-        vm.prank(nonOwner);
-
-        // Expect the transaction to revert
-        vm.expectRevert();
-        assignment.mintNFT(temporaryUriForTesting);
+    // دالة mint خارجية ومقيدة فقط بالمالك
+    function mintNFT(address to, string memory uri) external onlyOwner {
+        uint256 tokenId = _tokenIdCounter.current();
+        _mint(to, tokenId);
+        _setTokenURI(tokenId, uri);
+        _tokenIdCounter.increment();
     }
 }
